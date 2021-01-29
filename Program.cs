fbf7498b505978b5ed3d6f419b1e8c4cf0e4fe6b
hello <hello@LAPTOP-NA2F1EVT>
2021-01-29 11:34:25 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace CountApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter a number between 1 and 100");
+
+            try
+            {
+                string input = Console.ReadLine();
+                int value_of_input = int.Parse(input);
+                if ((value_of_input > 0) && (value_of_input <=100))
+                {
+                    Console.WriteLine("You have entered " + value_of_input.ToString());
+                    for (int i = 0; i < value_of_input; i++)
+                    {
+                        Console.WriteLine("The Current integer value in the loop : " + i.ToString());
+                    }
+                    Console.WriteLine("Press any key to exit the program");
+                    
+                    
+
+          
+                }
+            }
+            catch
+            {
+                Console.WriteLine("Please enter an integer value between 1 and 100");
+                Console.WriteLine("Please eneter any key to exit the program");
+                Console.ReadKey(true);
+                
+            }
+        }
+    }
+}
